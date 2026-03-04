bdcea5ac6ef22afbbfb924ccb71ee7585b58d64c
trondemynde <ronan.soot@gmail.com>
2026-03-04 15:12:24 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let todos = [
+  { id: 1, task: 'test 1' },
+  { id: 2, task: 'test 2' }
+];
+
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+  const todo = {
+    id: todos.length + 1,
+    task: req.body.task
+  };
+  todos.push(todo);
+  res.json(todo);
+});
+
+app.delete('/todos/:id', (req, res) => {
+  todos = todos.filter(t => t.id !== parseInt(req.params.id));
+  res.json({ message: 'Deleted' });
+});
+
+app.listen(3000);
